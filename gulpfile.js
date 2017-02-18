fd06342833ceb3203da453ad996472f0e22507f8
gfed53 <gfederico7153@gmail.com>
2017-02-18 15:56:57 -0500

    Minor tweaks in header, added gulp tasks, not currently usable

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var paths = {
+	sass: './sass/*.sass',
+	allSass: './sass/*/*.sass',
+	css: './css'
+};
+
+//CURRENT BUILD ISSUE, DO NOT USE
+
+// Compile SASS
+
+gulp.task('sass', function(){
+	return gulp.src(paths.sass)
+	.pipe(sass())
+	.pipe(gulp.dest(paths.css));
+});
+
+// Watch
+gulp.task('watch', function(){
+	gulp.watch(paths.allSass, ['sass']);
+});
+
+//Default
+gulp.task('default', ['sass', 'watch']);
