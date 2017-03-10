bdbdf318d6dbd18db6bcd10ab799090f1303fe23
gfed53 <gfederico7153@gmail.com>
2017-03-10 13:20:33 -0500

    Added velocityJS, animation to cta button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+/*jshint esversion: 6 */
+
 $(function(){
-	// $('.grid').css('backgroundColor', 'green');
+	console.log('hello ');
+
+	//CTA Button Animation
+
+	$('#btn-cta').on('mouseenter', ()=>{
+		// console.log('enter');
+		$('#btn-cta').velocity({
+			fontSize: '1.4em'
+		}, {
+			duration: 200
+		}
+		);
+	});
+
+	$('#btn-cta').on('mouseleave', ()=> {
+		// console.log('leave');
+		$('#btn-cta').velocity({
+			fontSize: '1.2em'
+		}, {
+			duration: 200
+		}
+		);
+	});
+
+
 });
