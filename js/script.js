a665bf60cf3374037eec75e7d0eb55c8a476fcb2
gfed53 <gfederico7153@gmail.com>
2017-03-10 14:04:10 -0500

    Added parallax scrolling for header bg, switched cta button to css3 animations for now, other minor tweaks

--- js/script.js
+++ js/script.js
@@ -3,27 +3,35 @@
 $(function(){
 	console.log('hello ');
 
+	//Parallax Header BG
+	$('#bg-container').fallings({
+		velocity: 0.2,
+		initialPosition: 0,
+		bgParallax: true,
+		bgPercent: "50%",
+		onClass: "fallings-visible",
+		offClass: "fallings-invisible"
+	});
+
 	//CTA Button Animation
 
-	$('#btn-cta').on('mouseenter', ()=>{
-		// console.log('enter');
-		$('#btn-cta').velocity({
-			fontSize: '1.4em'
-		}, {
-			duration: 200
-		}
-		);
-	});
+	// $('#btn-cta').on('mouseenter', ()=>{
+	// 	$('#btn-cta').velocity({
+	// 		fontSize: '1.4em'
+	// 	}, {
+	// 		duration: 200
+	// 	}
+	// 	);
+	// });
 
-	$('#btn-cta').on('mouseleave', ()=> {
-		// console.log('leave');
-		$('#btn-cta').velocity({
-			fontSize: '1.2em'
-		}, {
-			duration: 200
-		}
-		);
-	});
+	// $('#btn-cta').on('mouseleave', ()=> {
+	// 	$('#btn-cta').velocity({
+	// 		fontSize: '1.2em'
+	// 	}, {
+	// 		duration: 200
+	// 	}
+	// 	);
+	// });
 
 
 });
