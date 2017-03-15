c85131e8803b674ad37fe98ebe05407fa00e112c
gfed53 <gfederico7153@gmail.com>
2017-03-15 17:43:21 -0400

    various nav updates, add and removed animations, style updates

--- js/script.js
+++ js/script.js
@@ -3,68 +3,115 @@
 $(function(){
 	console.log('hello ');
 
-	//Parallax Header BG
-	$('#bg-container').fallings({
-		velocity: 0.2,
-		initialPosition: 0,
-		bgParallax: true,
-		bgPercent: "50%",
-		onClass: "fallings-visible",
-		offClass: "fallings-invisible"
-	});
+	function init(){
+		//Parallax Header BG
+		$('#bg-container').fallings({
+			velocity: 0.2,
+			initialPosition: 0,
+			bgParallax: true,
+			bgPercent: "50%",
+			onClass: "fallings-visible",
+			offClass: "fallings-invisible"
+		});
 
-	//Header Load Animation
-	$('#bg-container h1 span:nth-child(1)').velocity({
-		opacity: 1
-	}, {
-		duration: 1000
-	});
-	$('#bg-container h1 span:nth-child(2)').velocity({
-		opacity: 1
-	}, {
-		duration: 1500
-	});
-	$('#bg-container h1 span:nth-child(3)').velocity({
-		opacity: 1
-	}, {
-		duration: 2000
-	});
-	$('#bg-container h1 span:nth-child(4)').velocity({
-		opacity: 1
-	}, {
-		duration: 2500
-	});
-	$('#bg-container h1 span:nth-child(5)').velocity({
-		opacity: 1
-	}, {
-		duration: 3000
-	});
+		//Header Load Animation
+		$('#bg-container h1 span:nth-child(1)').velocity({
+			opacity: 1
+		}, {
+			duration: 1000
+		});
+		$('#bg-container h1 span:nth-child(2)').velocity({
+			opacity: 1
+		}, {
+			duration: 1500
+		});
+		$('#bg-container h1 span:nth-child(3)').velocity({
+			opacity: 1
+		}, {
+			duration: 2000
+		});
+		$('#bg-container h1 span:nth-child(4)').velocity({
+			opacity: 1
+		}, {
+			duration: 2500
+		});
+		$('#bg-container h1 span:nth-child(5)').velocity({
+			opacity: 1
+		}, {
+			duration: 3000
+		});
 
-	//Hamburger Nav Icon Animation
-	$('#nav-toggle').on('click', ()=> {
-		$('#nav-toggle').toggleClass('active');
-		$('.nav-links-mobile-container').toggleClass('hidden');
-	});
+		//Hamburger Nav Icon Animation
+		$('#nav-toggle').on('click', ()=> {
+			$('#nav-toggle').toggleClass('active');
+			$('.nav-links-mobile-container').toggleClass('active');
 
-	//CTA Button Animation
+			//Fade-in animation for nav contents
+			// $('.nav-links-mobile-container span').toggleClass('nav-item-hidden');
 
-	// $('#btn-cta').on('mouseenter', ()=>{
-	// 	$('#btn-cta').velocity({
-	// 		fontSize: '1.4em'
-	// 	}, {
-	// 		duration: 200
-	// 	}
-	// 	);
-	// });
+			// $('.nav-links-mobile-container ul li').toggleClass('nav-item-hidden');
 
-	// $('#btn-cta').on('mouseleave', ()=> {
-	// 	$('#btn-cta').velocity({
-	// 		fontSize: '1.2em'
-	// 	}, {
-	// 		duration: 200
-	// 	}
-	// 	);
-	// });
+			// $('.nav-links-mobile-container span').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 1000
+			// });
+			// $('.nav-links-mobile-container ul li:nth-child(1)').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 1500
+			// });
+			// $('.nav-links-mobile-container ul li:nth-child(2)').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 2000
+			// });
+			// $('.nav-links-mobile-container ul li:nth-child(3)').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 2500
+			// });
+			// $('.nav-links-mobile-container ul li:nth-child(4)').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 3000
+			// });
+			// $('.nav-links-mobile-container ul li:nth-child(5)').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 3500
+			// });
+			// $('.nav-links-mobile-container ul li:nth-child(6)').velocity({
+			// 	opacity: 1
+			// }, {
+			// 	duration: 4000
+			// });
+		});
 
+
+
+
+		//CTA Button Animation
+
+		// $('#btn-cta').on('mouseenter', ()=>{
+		// 	$('#btn-cta').velocity({
+		// 		fontSize: '1.4em'
+		// 	}, {
+		// 		duration: 200
+		// 	}
+		// 	);
+		// });
+
+		// $('#btn-cta').on('mouseleave', ()=> {
+		// 	$('#btn-cta').velocity({
+		// 		fontSize: '1.2em'
+		// 	}, {
+		// 		duration: 200
+		// 	}
+		// 	);
+		// });
+	}
+
+	init();
 
 });
