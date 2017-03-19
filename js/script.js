62f7ac4cdb55f2e42c37d26d2002b0841643e1d6
gfed53 <gfederico7153@gmail.com>
2017-03-19 13:23:45 -0400

    Added fade-in animation for mobile nav contents

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 
 $(function(){
-	console.log('hello ');
+	console.log($('.nav-links-mobile-container ul li'));
 
 	function init(){
 		//Parallax Header BG
@@ -54,6 +54,9 @@ $(function(){
 				setTimeout(()=>{
 					$('.nav-links-mobile-container').removeClass('enter').addClass('leave-active');
 				}, 250);
+				setTimeout(()=> {
+					navContentRemove();
+				}, 350);
 			} else {
 				$('.nav-links-mobile-container').removeClass('leave');
 				setTimeout(()=>{
@@ -62,48 +65,58 @@ $(function(){
 				setTimeout(()=>{
 					$('.nav-links-mobile-container').addClass('enter-active');
 				}, 100);
+				setTimeout(()=> {
+					navContentFadeIn();
+				}, 350);
 			}
 
 			//Fade-in animation for nav contents
+			function navContentFadeIn(){
+				$('.nav-links-mobile-container ul li:nth-child(1)').velocity({
+					opacity: 1
+				}, {
+					duration: 1000
+				});
+				$('.nav-links-mobile-container ul li:nth-child(2)').velocity({
+					opacity: 1
+				}, {
+					duration: 1000
+				});
+				$('.nav-links-mobile-container ul li:nth-child(3)').velocity({
+					opacity: 1
+				}, {
+					duration: 1000
+				});
+				$('.nav-links-mobile-container ul li:nth-child(4)').velocity({
+					opacity: 1
+				}, {
+					duration: 1500
+				});
+				$('.nav-links-mobile-container ul li:nth-child(5)').velocity({
+					opacity: 1
+				}, {
+					duration: 2000
+				});
+				$('.nav-links-mobile-container ul li:nth-child(6)').velocity({
+					opacity: 1
+				}, {
+					duration: 2000
+				});
+			}
+
+			function navContentRemove(){
+				$('.nav-links-mobile-container ul li').velocity({
+					opacity: 0
+				}, {
+					duration: 1000
+				});
+			}
 			// $('.nav-links-mobile-container span').toggleClass('nav-item-hidden');
 
 			// $('.nav-links-mobile-container ul li').toggleClass('nav-item-hidden');
 
-			// $('.nav-links-mobile-container span').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 1000
-			// });
-			// $('.nav-links-mobile-container ul li:nth-child(1)').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 1500
-			// });
-			// $('.nav-links-mobile-container ul li:nth-child(2)').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 2000
-			// });
-			// $('.nav-links-mobile-container ul li:nth-child(3)').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 2500
-			// });
-			// $('.nav-links-mobile-container ul li:nth-child(4)').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 3000
-			// });
-			// $('.nav-links-mobile-container ul li:nth-child(5)').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 3500
-			// });
-			// $('.nav-links-mobile-container ul li:nth-child(6)').velocity({
-			// 	opacity: 1
-			// }, {
-			// 	duration: 4000
-			// });
+			
+			
 		});
 
 
