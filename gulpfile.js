1d169efd9580711dc92490cefe86a03b61f89f54
gfed53 <gfederico7153@gmail.com>
2017-03-27 16:27:12 -0400

    Added autoprefixer to be used for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
 	sass: './sass/*.sass',
 	allSass: './sass/**/*.sass',
+	// preCSS: './sass/pre-css/pre-styles.css'
 	css: './css'
 };
 
@@ -14,6 +16,15 @@ gulp.task('sass', function(){
 	return gulp.src(paths.sass)
 	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(gulp.dest(paths.css));
+});
+
+gulp.task('autoprefixer', function() {
+    gulp.src('/css/styles.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(paths.css));
 });
 
 // Watch
